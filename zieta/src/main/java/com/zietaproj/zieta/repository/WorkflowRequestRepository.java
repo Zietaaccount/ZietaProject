45c8d875824a84fd8c457054e94a8a66523ee246
ranganathprn <ranganathprn@gmail.com>
2020-08-19 09:28:47 +0530

    implementing getallwf requests for requestor

--- zieta/src/main/java/com/zietaproj/zieta/repository/WorkflowRequestRepository.java
+++ zieta/src/main/java/com/zietaproj/zieta/repository/WorkflowRequestRepository.java
@@ -13,4 +13,6 @@ public interface WorkflowRequestRepository extends JpaRepository<WorkflowRequest
 	
 	public List<WorkflowRequest> findByApproverId(long approverId);
 	
+	public List<WorkflowRequest> findByRequestorId(long requestorId);
+	
 }
--- zieta/src/main/java/com/zietaproj/zieta/service/WorkFlowRequestService.java
+++ zieta/src/main/java/com/zietaproj/zieta/service/WorkFlowRequestService.java
@@ -3,11 +3,14 @@ package com.zietaproj.zieta.service;
 import java.util.List;
 
 import com.zietaproj.zieta.model.WorkflowRequest;
+import com.zietaproj.zieta.response.WorkFlowRequestorData;
 
 
 
 public interface WorkFlowRequestService {
 
 	public List<WorkflowRequest> findByApproverId(long approverId);
+	
+	public List<WorkFlowRequestorData> findByRequestorId(long requestorId);
 	
 }
