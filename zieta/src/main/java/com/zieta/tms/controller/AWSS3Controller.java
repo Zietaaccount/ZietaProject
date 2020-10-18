b3fa978212e3b37aba52337707195556e03f7822
ranganathprn <ranganathprn@gmail.com>
2020-10-18 15:30:01 +0530

    adding file attributes

--- zieta/src/main/java/com/zieta/tms/controller/AWSS3Controller.java
+++ zieta/src/main/java/com/zieta/tms/controller/AWSS3Controller.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.zieta.tms.request.S3AttributesModel;
 import com.zieta.tms.service.AWSS3Service;
 
 @RestController
@@ -22,8 +24,8 @@ public class AWSS3Controller{
 	private AWSS3Service service;
 
 	@PostMapping(value= "/s3/upload")
-	public ResponseEntity<String> uploadFile(@RequestPart(value= "file") MultipartFile multipartFile) {
-		service.uploadFile(multipartFile);
+	public ResponseEntity<String> uploadFile(@RequestPart(value= "multipartFile") MultipartFile multipartFile, @RequestParam String clientId  ) {
+		service.uploadFile(multipartFile, clientId);
 		final String response = "[" + multipartFile.getOriginalFilename() + "] uploaded successfully.";
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
--- zieta/src/main/java/com/zieta/tms/service/AWSS3Service.java
+++ zieta/src/main/java/com/zieta/tms/service/AWSS3Service.java
@@ -2,9 +2,11 @@ package com.zieta.tms.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.zieta.tms.request.S3AttributesModel;
+
 public interface AWSS3Service {
 
-	void uploadFile(MultipartFile multipartFile);
+	void uploadFile(MultipartFile multipartFile, String clietnId);
 	
 	byte[] downloadFile(String keyName);
 }
